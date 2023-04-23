a74469e454b6a84b3706157ec66787616d5af50d
Pablo Mera <pablomerakonrad@gmail.com>
2023-04-23 19:57:45 -0400

    Receta Medica

--- src/Pages/Receta/RecetaMedica.js
+++ src/Pages/Receta/RecetaMedica.js
@@ -1,323 +1,200 @@
-import * as React from 'react';
-
-import PropTypes from 'prop-types';
-
-import { alpha } from '@mui/material/styles';
-import { visuallyHidden } from '@mui/utils';
-
-// Components
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
-import Tooltip from '@mui/material/Tooltip';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-
-// Icons
-import AddIcon from '@mui/icons-material/Add';
-import DeleteIcon from '@mui/icons-material/Delete';
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import GetAppIcon from '@mui/icons-material/GetApp';
-import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
+import '../Medicamentos/Medicamentos.css'
+import React from "react";
+import { Box,Divider,Grid,InputBase,List,ListItem,ListItemText,ListSubheader,Paper,Button, Modal } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
+import { useState, useEffect } from "react";
 
-function createData(code, name, lab, reserva,fecha,icon) {
-    return { code, name, lab, reserva, fecha,icon };
+const _spacing = 2;
+
+const getItems = (number) => {
+    const items = [];
+    for(let i=0; i < number; i++){
+        items.push(
+            {
+                number: "P"+i,
+                text: "Lorem ipsum is simply dummy text",
+                key: i
+            }
+        );
+    }
+    return items;
 }
 
-const DEFAULT_ORDER = 'asc';
-const DEFAULT_ORDER_BY = 'code';
-const DEFAULT_ROWS_PER_PAGE = 10;
+const itemsList = getItems(15);
 
-const headCells = [
-  { id: 'code', numeric: true, disablePadding: true, label: 'Código' },
-  { id: 'name', numeric: false, disablePadding: false, label: 'Medicamento' },
-  { id: 'lab', numeric: false, disablePadding: false, label: 'Laboratorio' },
-  { id: 'reserva', numeric: true, disablePadding: false, label: 'Cantidad Reservada' },
-  { id: 'fecha', numeric: false, disablePadding: false, label: 'Fecha Llegada' },
+const Title = ({ children }) => {
+    return (
+        <p style={{fontWeight: "bold", margin: 0}}>{children}</p>
+    );
+}
+
+const CustomBox = ({ children, style }) => {
+    return (
+        <Paper sx={{padding: "0 1em", boxSizing: "content-box", background: "#F4EEE5", ...style}} elevation={0}>
+            {children}
+        </Paper>
+    );
+}
+
+const Field = (props) => {
+    return (
+        <Grid item xs={props.xs} md={props.md}>
+            <Title>{props.header}</Title>
+            <CustomBox>
+                <InputBase sx={{width: "100%"}}/>
+            </CustomBox>
+        </Grid>
+    )
+}
+
+const SearchBar = (props) => {
+    return (
+        <div style={{
+            background: "#A6D1E6",
+            color: "black",
+            display: "flex",
+            alignItems: "center",
+            gap: ".5em",
+            paddingLeft: "1em",
+        }}>
+            <SearchIcon />
+            <InputBase
+                placeholder={props.placeholder}
+                onChange={props.handleQuery}
+            />
+        </div>
+    );
+}
+
+const medicamentos = [
+    {name: 'Paracetamol 500mg', amount: 1, stock: true, state: 'Entregado'},
+    {name: 'Ibuproféno 600mg', amount: 1, stock: false, state: 'C. Paciente'},
+    {name: 'Tapsin', amount: 1, stock: false, state: 'Pendiente'},
 ];
 
-const rows = [  
-    createData(1324171354, 'Paracetamol','Laboratorio Chile S.A.', 11, '11-04-2023'),
-    createData(1403500365, 'Clorfenamina','Laboratorio Chile S.A.', 5, '11-04-2023'),
-    createData(1324171354, 'Ibuprofeto','Laboratorio Chile S.A.', 231, '11-04-2023'),
-    createData(1403500365, 'Betametasona','Mintlab Co. S.A.', 341, '11-04-2023'),
-    createData(1324171354, 'Ketorolaco','Laboratorio Chile S.A.', 4, '11-04-2023'),
-    createData(1403500365, 'Metanfetaminas','Laboratorio Chile S.A.', 312, '11-04-2023'),
-    createData(1324171354, 'Tapsin','Laboratorio Chile S.A.', 543, '11-04-2023'),
-    createData(1403500365, 'Clotrimazol','Mintlab Co. S.A.', 23, '11-04-2023'),
-];
+const Prescripciones = () => {
+    const s = (e,number) => {
+        if (hide === true) handleOpen();
+        let x = document.getElementsByClassName('itemSelected');
+        for (let i = 0; i< x.length; i++) {
+            x[i].classList.remove('itemSelected');
+        }
+        if (e.target.classList.contains('test')) e.target.className = 'test itemSelected';
+        else if (e.target.parentNode.classList.contains('test')) e.target.parentNode.className = 'test itemSelected';
+        else if (e.target.parentNode.parentNode.classList.contains('test')) e.target.parentNode.parentNode.className = 'test itemSelected';
+        else if (e.target.parentNode.parentNode.parentNode.classList.contains('test')) e.target.parentNode.parentNode.parentNode.className = 'test itemSelected';
+        else if (e.target.parentNode.parentNode.parentNode.parentNode.classList.contains('test')) e.target.parentNode.parentNode.parentNode.parentNode.className = 'test itemSelected';
 
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc' ? (a, b) => descendingComparator(a, b, orderBy) : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-function EnhancedTableHead(props) {
-  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
-  const createSortHandler = (newOrderBy) => (event) => {
-    onRequestSort(event, newOrderBy);
-  };
-
-  return (
-    <TableHead>
-      <TableRow bgcolor='#F4EEE5'> 
-        <TableCell padding="checkbox">
-          <Checkbox color="primary" indeterminate={numSelected > 0 && numSelected < rowCount} checked={rowCount > 0 && numSelected === rowCount} onChange={onSelectAllClick} inputProps={{ 'aria-label': 'Todos seleccionados' }} />
-        </TableCell>
-        {headCells.map((headCell) => (
-          <TableCell key={headCell.id} align={headCell.numeric ? 'right' : 'left'} padding={headCell.disablePadding ? 'none' : 'normal'} sortDirection={orderBy === headCell.id ? order : false} >
-            <TableSortLabel active={orderBy === headCell.id} direction={orderBy === headCell.id ? order : 'asc'} onClick={createSortHandler(headCell.id)}>
-              <b>{headCell.label}</b>
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </Box>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
-EnhancedTableHead.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
-
-function EnhancedTableToolbar(props) {
-  const { numSelected } = props;
-
-  return (
-    <Toolbar sx={{ pl: { sm: 2 }, pr: { xs: 1, sm: 1 }, ...(numSelected > 0 && { bgcolor: (theme) => alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),}), }} >
-      
-      {numSelected > 0 ? (
-        <Typography sx={{ flex: '1 1 100%' }} color="inherit" variant="subtitle1" component="div" >
-          {numSelected} seleccionado
-        </Typography>
-      ) : (
-        <Typography sx={{ flex: '1 1 100%' }} variant="h6" id="tableTitle" component="div" >
-          Listado
-        </Typography>
-      )}
-
-      {numSelected === 1 ? (
-        <Tooltip title="Acciones">
-          <IconButton>
-            <ModeEditOutlineIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <div></div>
-      )}
-
-      {numSelected >= 1 ? (
-        <div>
-        <Tooltip title="Delte">
-          <IconButton>
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      </div>
-      ) : (
-        <div>
-          <Tooltip title="Filter list">
-            <IconButton>
-              <FilterListIcon />
-            </IconButton>
-          </Tooltip>
+        //setTitle(number); 
+    };
+    const Item = (props) => {
+    return(
+        <div  onClick={(e) => s(e,props.number)} className="test">
+        <ListItem key={props.key}>
+            <ListItemText
+                primary={props.number}
+                primaryTypographyProps={{
+                    color: "grey",
+                }}
+                secondary={props.text}
+                secondaryTypographyProps={{
+                    color: "black",
+                    fontWeight: "bold"
+                }}
+            />
+        </ListItem>
+        <Divider/>
         </div>
-        
-      )}
-    </Toolbar>
-  );
-}
-
-EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-};
-
-export default function MedicamentosReservados () {
-  
-  const [order, setOrder] = React.useState(DEFAULT_ORDER);
-  const [orderBy, setOrderBy] = React.useState(DEFAULT_ORDER_BY);
-  const [selected, setSelected] = React.useState([]);
-  const [page, setPage] = React.useState(0);
-  const [dense] = React.useState(false);
-  const [visibleRows, setVisibleRows] = React.useState(null);
-  const [rowsPerPage, setRowsPerPage] = React.useState(DEFAULT_ROWS_PER_PAGE);
-  const [paddingHeight, setPaddingHeight] = React.useState(0);
-
-  React.useEffect(() => {
-    let rowsOnMount = stableSort(
-      rows,
-      getComparator(DEFAULT_ORDER, DEFAULT_ORDER_BY),
     );
+}
+    const [query, setQuery] = useState('');
+    const [hide, setHide] = useState(false);
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-    rowsOnMount = rowsOnMount.slice(
-      0 * DEFAULT_ROWS_PER_PAGE,
-      0 * DEFAULT_ROWS_PER_PAGE + DEFAULT_ROWS_PER_PAGE,
-    );
+    const _lg = 1200; // Default lg size
 
-    setVisibleRows(rowsOnMount);
-  }, []);
-
-  const handleRequestSort = React.useCallback(
-    (event, newOrderBy) => {
-      const isAsc = orderBy === newOrderBy && order === 'asc';
-      const toggledOrder = isAsc ? 'desc' : 'asc';
-      setOrder(toggledOrder);
-      setOrderBy(newOrderBy);
-
-      const sortedRows = stableSort(rows, getComparator(toggledOrder, newOrderBy));
-      const updatedRows = sortedRows.slice(
-        page * rowsPerPage,
-        page * rowsPerPage + rowsPerPage,
-      );
-
-      setVisibleRows(updatedRows);
-    },
-    [order, orderBy, page, rowsPerPage],
-  );
-
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelected = rows.map((n) => n.name);
-      setSelected(newSelected);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
+    const changeNavState = (sm) => {
+        const _show = window.innerWidth > sm ? false : true;
+        setHide(_show);
+        if (_show === false) handleClose();
+        console.log(hide);
     }
 
-    setSelected(newSelected);
-  };
-
-  const handleChangePage = React.useCallback(
-    (event, newPage) => {
-      setPage(newPage);
-
-      const sortedRows = stableSort(rows, getComparator(order, orderBy));
-      const updatedRows = sortedRows.slice(
-        newPage * rowsPerPage,
-        newPage * rowsPerPage + rowsPerPage,
-      );
-
-      setVisibleRows(updatedRows);
-
-      // Avoid a layout jump when reaching the last page with empty rows.
-      const numEmptyRows = newPage > 0 ? Math.max(0, (1 + newPage) * rowsPerPage - rows.length) : 0;
-
-      const newPaddingHeight = (dense ? 33 : 53) * numEmptyRows;
-      setPaddingHeight(newPaddingHeight);
-    },
-    [order, orderBy, dense, rowsPerPage],
-  );
-
-  const handleChangeRowsPerPage = React.useCallback(
-    (event) => {
-      const updatedRowsPerPage = parseInt(event.target.value, 10);
-      setRowsPerPage(updatedRowsPerPage);
-
-      setPage(0);
-
-      const sortedRows = stableSort(rows, getComparator(order, orderBy));
-      const updatedRows = sortedRows.slice(
-        0 * updatedRowsPerPage,
-        0 * updatedRowsPerPage + updatedRowsPerPage,
-      );
-
-      setVisibleRows(updatedRows);
-
-      // There is no layout jump to handle on the first page.
-      setPaddingHeight(0);
-    },
-    [order, orderBy],
-  );
-
-  const isSelected = (name) => selected.indexOf(name) !== -1;
-
-  return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }} style={{margin: '20px', padding: '20px'}}>
+    window.onload = () => {
+        changeNavState(_lg);
+    };
     
-        <h1>Receta Médica Electrónica</h1>
-        
-        <div style={{marginBottom: '20px', color: 'black', borderRadius: '5px', display: 'inline-flex',width: '100%'}}>
-            <Button variant="contained" style={{backgroundColor: '#F4EEE5', color: 'black', marginRight: '10px', boxShadow: 'none' }}><FilterAltIcon /></Button>
-            <div style={{backgroundColor: '#F4EEE5', width: '100%', borderRadius: '5px', marginRight: '10px'}}>
-                <IconButton type="button" sx={{ p: '10px' }} aria-label="search" >
-                    <SearchIcon />
-                </IconButton>
-                <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Buscar..." inputProps={{ 'aria-label': 'Buscar...' }}  />
-            </div>
-            <Button variant="contained" style={{backgroundColor: '#F4EEE5', color: 'black', boxShadow: 'none' }}><AddIcon /></Button>
-        </div>
-        <div style={{marginBottom: '20px', color: 'black', borderRadius: '5px', display: 'inline-flex',width: '100%'}}>
-            <div style={{backgroundColor: '#F4EEE5', width: '100%', borderRadius: '5px', marginRight: '10px'}}>
-                <IconButton type="button" sx={{ p: '10px' }} aria-label="search" >
-                    <SearchIcon />
-                </IconButton>
-                <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Buscar..." inputProps={{ 'aria-label': 'Buscar...' }}  />
-            </div>
-        </div>
-        
-    
+    useEffect(() => {
+        window.addEventListener('resize', () => {
+            changeNavState(_lg);
+        });
 
-        <Button style={{display: 'flex', float: 'right'}} variant="contained" startIcon={<GetAppIcon />}> Generar Reporte</Button>
-    </Paper>);
-}   
+        if(window.innerWidth < _lg) {
+            setHide(true);
+        }
+    });
+
+    const handleQuery = (e) => {
+        setQuery(e.target.value);
+    }
+
+    return (
+        <Box sx={{ flexGrow: 1}}>
+                <Grid item xs={12} lg={8} p={_spacing} >
+                    <Grid container spacing={_spacing} sx={{display: hide ? 'none' : 'inherith'}}>
+                        <Grid item xs={12}><h1>Receta Médica Electrónica</h1></Grid>
+                        <Field xs={6} header={"Identificador"}/>
+                        <Field xs={6} header={"Fecha de Emisión"}/>
+                        <Field xs={12} header={"Nombre Completo del Paciente"}/>
+                        <Field xs={6} header={"RUN"}/>
+                        <Field xs={6} header={"Fecha de Nacimiento"}/>
+                        <Field xs={12} header={"Edad"}/>
+                        <Grid item xs={12}>
+                            <Title>Prescripción</Title>
+                            <Grid container spacing={_spacing}>
+                                <Grid item xs={10}>
+                                    <CustomBox style={{height: "2em", display: "flex", alignItems: "center"}}>
+                                        <p><b>Medicamento</b></p>
+                                    </CustomBox>
+                                    {medicamentos.map(medicamento => {
+                                        return (
+                                            <CustomBox style={{height: "2em", marginTop: ".2em", display: "flex", alignItems: "center"}}>
+                                                <Field  />
+                                            </CustomBox>
+                                        )
+                                    })}
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <CustomBox style={{height: "2em", display: "flex", alignItems: "center"}}>
+                                        <p><b>Cantidad</b></p>
+                                    </CustomBox>
+                                    {medicamentos.map(medicamento => {
+                                        return (
+                                            <CustomBox style={{height: "2em", marginTop: ".2em", display: "flex", alignItems: "center"}}>
+                                                <Field  />
+                                            </CustomBox>
+                                        )
+                                    })}
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <p>
+                                <b>Dr. Federico Santa María</b><br></br>
+                                RUN 11.111.111-1<br></br>
+                                Especialidad: Médico Cirujano
+                            </p>
+                        </Grid>
+                        <Grid item xs={2}></Grid>
+                        <Grid item xs={2}>
+                            <Button variant="contained" disableElevation fullWidth style={{marginTop: "1em"}}>Emitir</Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+        </Box>
+    )
+}
+
+export default Prescripciones;
